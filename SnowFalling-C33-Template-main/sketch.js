7d5c04bf3cdc633174ca00f3ea19b30698a980ec
AADESH-CELL <70965444+AADESH-CELL@users.noreply.github.com>
2021-06-28 17:01:25 +0530

    Add files via upload

---
+++ SnowFalling-C33-Template-main/sketch.js
@@ -0,0 +1,18 @@
+var boy,boyImage
+var bg
+var snow,snowImage
+function preload (){
+boy
+
+
+}
+ function setup() {
+
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
